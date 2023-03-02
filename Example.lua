f3fc407dd098b1ebc79f7704a08e118bd06026a6
OopssSorry <96795888+OopssSorry@users.noreply.github.com>
2023-03-02 10:37:40 +0200

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,14 @@
+_G._,_, Discord = 'discord.gg/enmKTMQvFJ'
+
+,(function() loadstring(game:HttpGet("oopshub.vercel.app/libs/KeySystem1.lua"))():KeySystem({
+--[[
+		OopssKeySystemV2
+		Paste your script after this code
+		Discord get key links support
+	
+		Settings:
+]]Name = "your_app_name", -- put here name(For save key file name)
+	GetKeyLink = "https://1.kelprepl.repl.co/getkey/your_app_name", -- you can put here discord link
+	VerifyLink = "https://1.kelprepl.repl.co/verify/your_app_name?verify_key=",-- KEY == game:HttpGet("https://1.kelprepl.repl.co/verify/your_app_name?verify_key="..KEY)
+	SaveKey = true, -- just save key
+	});end),[['..==LOL..']];_()
